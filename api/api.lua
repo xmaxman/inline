628dcc6efabd8b3f3895378061c094eb2b1b8053
TelePlus Team <shahradq@gmail.com>
2017-03-24 16:13:44 +0430

    TelePlus v2

--- api/api.lua
+++ api/api.lua
@@ -1,7 +1,7 @@
 local URL = require "socket.url"
 local https = require "ssl.https"
 local serpent = require "serpent"
-local json = (loadfile "/home/root/data/JSON.lua")()
+local json = (loadfile "/username/data/JSON.lua")()
 local token = '' --token
 local url = 'https://api.telegram.org/bot' .. token
 local offset = 0
